d70ebd9441679db522b9fc66f1560229848628ad
Zain Ali <zain534102@gmail.com>
2021-06-04 19:42:42 +0500

    feat:test task

--- src/app/post/models/post.ts
+++ src/app/post/models/post.ts
@@ -1,9 +1,12 @@
-export class Post{
-    userId!:number;
-    id!:number;
-    title!:string;
-    body!:string
+export class Post {
+    userId!: number;
+    id!: number;
+    title!: string;
+    body!: string;
 }
-export class PostResponse{
-    data!:Post[];
+export class PostResponse {
+    data!: Post[];
+}
+export class PosCreatedResponse {
+    id!: number;
 }
--- src/app/post/services/post.service.ts
+++ src/app/post/services/post.service.ts
@@ -1,20 +1,24 @@
-import { Post,PostResponse } from './../models/post';
+import { Post,PostResponse,PosCreatedResponse } from './../models/post';
 import { environment } from './../../../environments/environment';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 @Injectable({
     providedIn: 'root'
 })
 export class PostService{
-    
-    private getUrl = environment.REST_HOST+'posts';
-    private createUrl = environment.REST_HOST+'posts';
+
+    private getUrl = environment.REST_HOST + 'posts';
+    private createUrl = environment.REST_HOST + 'posts';
+    private headers = new HttpHeaders()
+            .set('Content-type', 'application/json; charset=UTF-8');
     constructor(
         private http: HttpClient
       ) { }
-        protected getAllPosts():Observable<PostResponse>{
+        protected getAllPosts(): Observable <PostResponse>{
             return this.http.get<PostResponse>(this.getUrl);
         }
-        protected createPost()
+        protected createPost(request: any): Observable<any>{
+            return this.http.post(this.createUrl, {headers: this.headers}, request);
+        }
 }
